48ddd9bca8f4c2042bd6b2d785f30cfe564031f0
LeaveMyYard <mrdiamondpavel@gmail.com>
2021-05-31 11:43:29 +0300

    Update user.py

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -7,13 +7,8 @@ from pydantic import Field
 
 
 class UserCreate(APIModel):
-    nickname: str = Field(
-        regex=r"^([A-Za-zÄäÖöÜüß].{0}[A-Za-z0-9._ÄäÖöÜüß]{0,17}.[^._])$", max_length=20
-    )
-    password: str = Field(
-        regex=r'^(?!.*[ .])(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,32}(?=.+[!&$%&?"]){0}.*$',  # https://regex101.com/
-        max_length=50,
-    )
+    nickname: str
+    password: str
 
 
 class UserUpdate(APIModel):
